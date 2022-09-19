a6361c7c4eb662071fd034533b1d76ccbc0f7858
Ykyki4 <jimmypantalones@gmail.com>
2022-09-19 10:19:05 +0300

    Made code cleaner

--- vkcomics.py
+++ vkcomics.py
@@ -16,6 +16,7 @@ def download_comics_image(image_url, comics_name):
         file.write(response.content)
         return path
 
+
 def post_image_to_server(path, upload_url):
     try:
         with open(path, 'rb') as file:
@@ -43,12 +44,13 @@ def post_image_to_album(image_hash, server_id, photo, access_token, api_version)
 
 def post_image_to_wall(group_id, owner_id, media_id, api_version, commics_message):
     url = "https://api.vk.com/method/wall.post"
-    params = {"access_token": access_token,
-              "v": api_version,
-              "attachments": f"photo{owner_id}_{media_id}",
-              "owner_id": group_id,
-              "from_group": 1,
-              "message": commics_message
+    params = {
+        "access_token": access_token,
+        "v": api_version,
+        "attachments": f"photo{owner_id}_{media_id}",
+        "owner_id": group_id,
+        "from_group": 1,
+        "message": commics_message
     }
     response = requests.post(url, params=params)
     response.raise_for_status()
@@ -57,8 +59,10 @@ def post_image_to_wall(group_id, owner_id, media_id, api_version, commics_messag
 
 def request_upload_url(access_token, api_version):
     url = f"https://api.vk.com/method/photos.getWallUploadServer"
-    params = {"access_token": access_token,
-              "v": api_version}
+    params = {
+        "access_token": access_token,
+        "v": api_version
+    }
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.json()["response"]["upload_url"]
@@ -78,25 +82,40 @@ if __name__ == "__main__":
     comics_response = response.json()
 
     upload_url = request_upload_url(
-        access_token, api_version)
+        access_token,
+        api_version
+        )
     image_url = comics_response["img"]
 
     comics_name = comics_response["safe_title"]
     comics_image_path = download_comics_image(image_url, comics_name)
 
     server_post_response = post_image_to_server(
-        comics_image_path, upload_url)
+        comics_image_path,
+        upload_url
+        )
 
     server_post_hash = server_post_response["hash"]
     server_post_photo = server_post_response["photo"]
     server_post_id = server_post_response["server"]
 
     album_post_response = post_image_to_album(
-        server_post_hash, server_post_id, server_post_photo, access_token, api_version)
+        server_post_hash,
+        server_post_id, 
+        server_post_photo, 
+        access_token, 
+        api_version
+        )
     commics_message = comics_response["alt"]
     owner_id = album_post_response["response"][0]["owner_id"]
     media_id = album_post_response["response"][0]["id"]
 
 
     print(post_image_to_wall(
-        group_id, owner_id, media_id, api_version, commics_message))
+        group_id, 
+        owner_id, 
+        media_id, 
+        api_version, 
+        commics_message
+        )
+    )
